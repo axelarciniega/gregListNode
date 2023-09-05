b10b8dc2708173bcabf2ad17e240a18572ba7694
AxelArciniega <axelarciniega12@icloud.com>
2023-09-05 16:33:06 -0600

    get cars

--- server/services/HouseService.js
+++ server/services/HouseService.js
@@ -1,6 +1,15 @@
 import { dbContext } from "../db/DbContext.js"
 
 class HouseService{
+
+    async getHouses(){
+        const houses = await dbContext.House.find()
+        return houses 
+    }
+
+
+
+
     async createHouse(houseId) {
         const house = await dbContext.House.create(houseId)
         return house
